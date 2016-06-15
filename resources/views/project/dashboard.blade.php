63dfb8e7f2d9b8da91b235cc1cd1a71ba1d33fa6
Michal Nosavcov <michal.nosavcov@economia.cz>
2016-06-15 10:04:59 +0200

    update project.dashboard template

--- resources/views/project/dashboard.blade.php
+++ resources/views/project/dashboard.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <a href="{{ Route('project.detail', ['id'=>$project->key]) }}">Detail</a>
-            <a href="{{ Route('project.update', ['id'=>$project->key]) }}">Update</a>
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="{{ Route('project.detail', ['id'=>$project->key]) }}">Detail</a>
+                <a href="{{ Route('project.update', ['id'=>$project->key]) }}">Update</a>
+            </div>
         </div>
     </div>
 
     <div class="row">
         <div class="col-sm-4">
-            <div class="row" style="padding-bottom: 8px;">
-                <div class="col-sm-12">
-                    <div class="label label-default"
-                         style="width: auto; display: block; padding: 10px 15px; text-align: left;">todo
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="label label-default">todo</div>
                     </div>
                 </div>
             </div>
@@ -33,10 +35,10 @@
             @endforeach
         </div>
         <div class="col-sm-4">
-            <div class="row" style="padding-bottom: 8px;">
-                <div class="col-sm-12">
-                    <div class="label label-default"
-                         style="width: auto; display: block; padding: 10px 15px; text-align: left;">in work
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="label label-default">in progress</div>
                     </div>
                 </div>
             </div>
@@ -56,10 +58,10 @@
             @endforeach
         </div>
         <div class="col-sm-4">
-            <div class="row" style="padding-bottom: 8px;">
-                <div class="col-sm-12">
-                    <div class="label label-default"
-                         style="width: auto; display: block; padding: 10px 15px; text-align: left;">done / reject
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="label label-default">done / reject</div>
                     </div>
                 </div>
             </div>
