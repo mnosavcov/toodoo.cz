9b99bf53f40a6473e72f1783a1fea518f29e8fc9
Michal Nosavcov <michal.nosavcov@economia.cz>
2016-06-30 10:04:10 +0200

    added version; added row break

--- resources/views/@shared/taskbox.blade.php
+++ resources/views/@shared/taskbox.blade.php
@@ -1,1 +1,6 @@
-<div class="bs-callout bs-callout-danger" id="callout-overview-not-both"><h4><a href="{{ route('task.detail', ['key'=>$task->key()]) }}" style="color: inherit">{{ $task->name }}</a></h4><p>{{ $task->description }}</p></div>
+<div class="bs-callout bs-callout-danger" id="callout-overview-not-both">
+    <h4>
+        <a href="{{ route('task.detail', ['key'=>$task->key()]) }}" style="color: inherit">{{ $task->name }}</a>
+    </h4>
+    <p>{!! nl2br(nl2br(e($task->description))) !!}</p>
+</div>
--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -6,6 +6,8 @@
             <div class="col-sm-12 col-md-12">
                 <h1>Welcome</h1>
                 <p>Pro fungování je třeba <a href="{{ url('login') }}">přihlásit se</a></p>
+
+                <div>verze {{ config('app.version') }}</div>
             </div>
         </div>
     </div>
