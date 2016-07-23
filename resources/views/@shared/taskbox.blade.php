5123ea11d60532c56d34c1e7b1fc24d69aac523d
Michal Nosavcov <mnosavcov@gmail.com>
2016-07-23 19:49:18 +0200

    updated styles

--- resources/views/@shared/taskbox.blade.php
+++ resources/views/@shared/taskbox.blade.php
@@ -1,23 +1,17 @@
 <div class="bs-callout bs-callout{{ $task->priority }}" id="callout-overview-not-both">
-    <div class="navbar-wrap">
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle btn btn-default" data-toggle="dropdown" role="button" aria-expanded="false">
-                        <span class="glyphicon glyphicon-option-horizontal"></span>
-                        <span class="sr-only">Toggle Dropdown</span>
-                    </a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <a href="{{ route('task.update', ['key'=>$task->key()]) }}">
-                                <span class="glyphicon glyphicon-pencil"></span>
-                                &nbsp;Upravit
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+    <div class="dropdown pull-right">
+        <a href="#" class="dropdown-toggle btn btn-default" data-toggle="dropdown" role="button" aria-expanded="false">
+            <span class="glyphicon glyphicon-option-horizontal"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+        </a>
+        <ul class="dropdown-menu" role="menu">
+            <li>
+                <a href="{{ route('task.update', ['key'=>$task->key()]) }}">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                    &nbsp;Upravit
+                </a>
+            </li>
+        </ul>
     </div>
     <h4 class="block-with-text">
         <a href="{{ route('task.detail', ['key'=>$task->key()]) }}" style="color: inherit"
