4aa71bd0ea39330ed8fd96237b40a3e0056cbe97
Michal Nosavcov <mnosavcov@gmail.com>
2016-07-23 12:05:16 +0200

    updated styles of task boxes

--- resources/views/@shared/taskbox.blade.php
+++ resources/views/@shared/taskbox.blade.php
@@ -1,6 +1,6 @@
 <div class="bs-callout bs-callout{{ $task->priority }}" id="callout-overview-not-both">
     <h4>
-        <a href="{{ route('task.detail', ['key'=>$task->key()]) }}" style="color: inherit">{{ $task->name }}</a>
+        <a href="{{ route('task.detail', ['key'=>$task->key()]) }}" style="color: inherit" title="{{ $task->name }}">{{ $task->name }}</a>
     </h4>
-    <p>{!! nl2br(linkInText(e($task->description))) !!}</p>
+    <p class="description">{!! nl2br(linkInText(e($task->description))) !!}</p>
 </div>
