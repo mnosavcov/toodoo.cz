f46f1060a872936f1e6487e8f8ac344f1a50c4dc
Michal Nosavcov <mnosavcov@gmail.com>
2016-07-30 18:45:50 +0200

    add helper function formatBytes + style for non image thumbs

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -22,3 +22,16 @@ function linkInText($text)
     return preg_replace_callback("&\\b$rexProtocol$rexDomain$rexPort$rexPath$rexQuery$rexFragment(?=[?.!,;:\"]?(\s|$))&",
         'linkInText_callback', $text);
 }
+
+function formatBytes($size, $precision = 2)
+{
+    if ($size > 0) {
+        $size = (int) $size;
+        $base = log($size) / log(1024);
+        $suffixes = array(' bytes', ' KB', ' MB', ' GB', ' TB');
+
+        return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+    } else {
+        return $size;
+    }
+}
--- resources/views/@shared/thumb.blade.php
+++ resources/views/@shared/thumb.blade.php
@@ -5,8 +5,10 @@
                 <img class="img-thumbnail"
                      src="{{ $file->thumb }}">
             @else
-                <div class="img-thumbnail" style="width: 140px; height: 140px;">
-                    {{ $file->extname }}
+                <div class="img-thumbnail">
+                    <div class="in">
+                        {{ $file->extname }}
+                    </div>
                 </div>
             @endif
         </a>
