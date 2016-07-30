7be12e9fa650dcf7cbe92d2cf08e1372457eb989
Michal Nosavcov <mnosavcov@gmail.com>
2016-07-30 10:51:03 +0200

    update tables

---
+++ database/migrations/2016_07_30_084326_add_filesize_to_task_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFilesizeToTaskFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('task_files', function (Blueprint $table) {
+            $table->integer('filesize')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('task_files', function (Blueprint $table) {
+            $table->dropColumn('filesize');
+        });
+    }
+}
---
+++ database/migrations/2016_07_30_104854_create_project_files_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_files', function (Blueprint $table) {
+	        $table->engine = 'InnoDB';
+
+	        $table->increments('id');
+	        $table->integer('created_at');
+	        $table->integer('updated_at');
+
+	        $table->integer('project_id')->unsigned();
+
+	        $table->string('ftp_connection');
+	        $table->string('file_md5', 32);
+	        $table->string('fullfile', 255);
+	        $table->string('pathname', 255);
+	        $table->string('filename', 255);
+	        $table->string('extname', 32);
+	        $table->string('mime_type', 255);
+	        $table->string('thumb', 255)->nullable();
+            $table->integer('filesize')->unsigned();
+
+	        $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::drop('project_files');
+    }
+}
