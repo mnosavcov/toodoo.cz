f4c9284977286c000abefa159387591b37b3c3d6
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-06 08:31:53 +0200

    add dev FTP; update decrypt in project and tast form

--- config/ftp.php
+++ config/ftp.php
@@ -11,7 +11,7 @@ return array(
 	|
 	*/
 
-    'default' => 's18655',
+    'default' => env('FTP_default', 's18784'), // default = DEV
 
     /*
     |--------------------------------------------------------------------------
@@ -24,6 +24,13 @@ return array(
 
     'connections' => array(
 
+        's18784' => array( // DEV
+            'host'   => '18784.s84.wedos.net',
+            'port'  => 21,
+            'username' => 's18784',
+            'password'   => env('FTP_s18784_PWD'),
+            'passive'   => false,
+        ),
         's18655' => array(
             'host'   => '18655.s55.wedos.net',
             'port'  => 21,
