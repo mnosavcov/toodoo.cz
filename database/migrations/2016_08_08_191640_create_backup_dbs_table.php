997016d447a3a87cbb440d41ce7dbaf97162cbb0
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-09 13:21:54 +0200

    repair DB migrate

--- database/migrations/2016_08_08_191640_create_backup_dbs_table.php
+++ database/migrations/2016_08_08_191640_create_backup_dbs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBackupDbTable extends Migration
+class CreateBackupDbsTable extends Migration
 {
     /**
      * Run the migrations.
