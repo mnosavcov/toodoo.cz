4de76a5d6ea201a488eaefa0a5ba396077947c2b
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-15 07:58:39 +0200

    update email config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'info@toodoo.cz', 'name' => 'TooDoo.cz'],
 
     /*
     |--------------------------------------------------------------------------
