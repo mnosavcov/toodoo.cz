16badfe190eda812ed41846d9e1211dad14f0ddd
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-16 23:50:18 +0200

    translate

--- resources/lang/cz/passwords.php
+++ resources/lang/cz/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Heslo musí mít minimálně 6 znaků a musé souhlasit kontrola hesla.',
+    'reset' => 'Vaše heslo bylo změněno!',
+    'sent' => 'Na Váš email byl odeslán odkaz na změnu hesla!',
+    'token' => 'Token pro změnu hesla není validní.',
+    'user' => "Uživatel s tímto emailem neexistuje.",
 
 ];
