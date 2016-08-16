6fc1f75c714eace70540ffd6a35c9d165f849047
Michal Nosavcov <michal.nosavcov@economia.cz>
2016-08-16 13:45:44 +0200

    repair plain email

--- resources/views/auth/emails/password-text.blade.php
+++ resources/views/auth/emails/password-text.blade.php
@@ -1,3 +1,7 @@
-Na základě Vašeho požadavku Vám zasíláme odkaz pro obnovení hesla.
-Zkopírujte přiložený odkaz do prohlížeče
-{{ url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}
+@extends('layouts.email-text')
+
+@section('content')
+    Na základě Vašeho požadavku Vám zasíláme odkaz pro obnovení hesla.
+    Zkopírujte přiložený odkaz do prohlížeče
+    {{ url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}
+@endsection
