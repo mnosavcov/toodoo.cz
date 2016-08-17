6c3f5daea157fea8d58a5d63050ebcaad22ca6ac
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-17 21:38:14 +0200

    translates

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2 main">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading">Obnovit heslo</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
