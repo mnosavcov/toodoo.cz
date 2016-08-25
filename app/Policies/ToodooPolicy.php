8cdf22aba4afe1cc28c7fe4ce2c7c1df50e023f5
Michal Nosavcov <michal.nosavcov@economia.cz>
2016-08-25 10:53:58 +0200

    update policies

--- app/Policies/ToodooPolicy.php
+++ app/Policies/ToodooPolicy.php
@@ -5,9 +5,22 @@ namespace App\Policies;
 use App\User;
 use App\Project;
 use App\Task;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ToodooPolicy
 {
+	use HandlesAuthorization;
+
+	/**
+	 * Create a new policy instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
     public function updateProject(User $user, Project $project)
     {
         return $user->id === $project->user_id;
