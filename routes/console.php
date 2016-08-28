077d573b0ba4d8b9774bf7bb8b087e67c889883d
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-28 13:36:20 +0200

    update command

--- routes/console.php
+++ routes/console.php
@@ -23,8 +23,8 @@ Artisan::command('publish {--migrate}', function () {
         exec('php artisan migrate --force');
     }
     exec('php artisan cache:clear');
-    exec('rm ' . config('view.compiled') . DIRECTORY_SEPARATOR . '*.php');
     exec('rm ' . base_path() . DIRECTORY_SEPARATOR . 'bootstrap' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . '*.php');
+    exec('rm ' . config('view.compiled') . DIRECTORY_SEPARATOR . '*.php');
 });
 
 Artisan::command('run', function () {
