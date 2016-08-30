d4aedbc5e17ddd29d0b84d6bc949bef060d2c199
Michal Nosavcov <mnosavcov@gmail.com>
2016-08-30 20:57:16 +0200

    migrations updated

--- database/migrations/2016_08_30_135446_update_users_table_01.php
+++ database/migrations/2016_08_30_135446_update_users_table_01.php
@@ -13,12 +13,29 @@ class UpdateUsersTable01 extends Migration
      */
     public function up()
     {
-	    DB::statement("ALTER TABLE users CHANGE COLUMN purchased_size paid_size BIGINT(20) unsigned NOT NULL");
+	    Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('purchased_size');
+            $table->bigInteger('paid_size')->unsigned()->after('main_size');
 
-//	    Schema::table('users', function (Blueprint $table) {
-//
-//		    //$table->renameColumn('purchased_size', 'paid_size');
-//	    });
+            $table->dropColumn('purchase_expire_at');
+            $table->integer('paid_expire_at')->after('paid_size');
+
+            $table->bigInteger('ordered_unpaid_size')->unsigned()->after('paid_expire_at');
+            $table->integer('ordered_unpaid_expire_at')->after('ordered_unpaid_size');
+
+            $table->dropColumn('order_size');
+            $table->bigInteger('ordered_size')->unsigned()->after('free_size');
+
+            $table->dropColumn('order_period');
+            $table->enum('ordered_period', ['monthly', 'yearly'])->default('yearly')->after('ordered_size');
+
+            $table->dropColumn('renew_active');
+
+	    });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('renew_active')->default(0)->after('ordered_period');
+        });
     }
 
     /**
@@ -28,9 +45,24 @@ class UpdateUsersTable01 extends Migration
      */
     public function down()
     {
-	    DB::statement("ALTER TABLE users CHANGE COLUMN paid_size purchased_size BIGINT(20) unsigned NOT NULL");
-//	    Schema::table('users', function (Blueprint $table) {
-//		    //$table->renameColumn('paid_size', 'purchased_size');
-//	    });
+	    //DB::statement("ALTER TABLE users CHANGE COLUMN paid_size purchased_size BIGINT(20) unsigned NOT NULL");
+	    Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('paid_size');
+            $table->bigInteger('purchased_size')->unsigned()->after('main_size');
+
+            $table->dropColumn('paid_expire_at');
+            $table->integer('purchase_expire_at')->after('purchased_size');
+
+            $table->dropColumn('ordered_unpaid_size');
+            $table->dropColumn('ordered_unpaid_expire_at');
+
+            $table->dropColumn('ordered_size');
+            $table->bigInteger('order_size')->unsigned();
+
+            $table->dropColumn('ordered_period');
+            $table->enum('order_period', ['monthly', 'yearly'])->default('yearly');
+	    });
+
+        // renew_active is not need drop, in up method is only moved
     }
 }
