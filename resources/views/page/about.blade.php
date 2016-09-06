4a8c01106cc641829f0e845d9ec82e0847c6caed
Michal Nosavcov <mnosavcov@gmail.com>
2016-09-06 21:38:00 +0200

    repair about page

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,36 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-8">
-            <h1>O projektu</h1>
-            <p>
-                Projek je vytvořený pro snadnou správu projektů a úkolů.
-            </p>
+    <h1>O projektu</h1>
+    <p>
+        Projek je vytvořený pro snadnou správu projektů a úkolů.
+    </p>
 
-            <h2>Bezpečnost</h2>
-            <p>
-                Bezpečnost je pro nás důležitá i proto běží web na https protokolu, který zajišťuje, že při komunikaci
-                se
-                serverem není možné data odposlouchat ani je cestou změnit.<br>
-                <br>
-                <strong>
-                    Další bezpečnostní prvek je možnost vytvořit skrytý popis u projektu i úkolu, který se nejen
-                    zobrazuje na vyžádání kliknutím na odkaz zobrazit, ale data ve skrytém popisu jsou v úložišti jsou
-                    navíc kryptována tak, že není možné je přečíst, ani rozkódovat.
-                </strong>
-            </p>
+    <h2>Bezpečnost</h2>
+    <p>
+        Bezpečnost je pro nás důležitá i proto běží web na https protokolu, který zajišťuje, že při komunikaci
+        se
+        serverem není možné data odposlouchat ani je cestou změnit.<br>
+        <br>
+        <strong>
+            Další bezpečnostní prvek je možnost vytvořit skrytý popis u projektu i úkolu, který se nejen
+            zobrazuje na vyžádání kliknutím na odkaz zobrazit, ale data ve skrytém popisu jsou v úložišti jsou
+            navíc kryptována tak, že není možné je přečíst, ani rozkódovat.
+        </strong>
+    </p>
 
-            <h2>Cena</h2>
-            <p>
-                Užívání portálu a všech jeho součástí je zdarma. Místo pro Vaše soubory je však omezené a je možné
-                dokoupit
-                si
-                další prostor. Nicméně není povinné si místo pořídit a soubory je možné vložit na jiné úložiště a do
-                úkolu
-                nebo
-                projektu vložit odkaz na soubory popisu.
-            </p>
-        </div>
-    </div>
+    <h2>Cena</h2>
+    <p>
+        Užívání portálu a všech jeho součástí je zdarma. Místo pro Vaše soubory je však omezené a je možné
+        dokoupit
+        si
+        další prostor. Nicméně není povinné si místo pořídit a soubory je možné vložit na jiné úložiště a do
+        úkolu
+        nebo
+        projektu vložit odkaz na soubory popisu.
+    </p>
 @endsection
